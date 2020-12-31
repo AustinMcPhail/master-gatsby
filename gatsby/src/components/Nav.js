d69dc624e27b938124c8b4cfde70e786ef52b6a4
Austin McPhail <austin@mcphail.dev>
2020-12-31 12:00:27 -0600

    feat: 🎸 more layout

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,15 +1,60 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, navigate } from 'gatsby'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import Logo from './Logo'
 
-function goSliceMasters() {
-  setTimeout(() => {
-    navigate('/beers')
-  }, 2000)
-}
+const NavStyled = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+
+  ul {
+    margin: 0;
+    margin-top: clamp(-3rem, 1vw, -5rem);
+    padding: 0;
+    text-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+
+    li {
+      --r: -2.5deg;
+      transform: rotate(var(--r));
+      order: 1;
+      &:nth-child(1) {
+        --r: 2.5deg;
+      }
+      &:nth-child(2) {
+        --r: -2.5deg;
+      }
+      &:nth-child(4) {
+        --r: 2.5deg;
+      }
+
+      &:hover {
+        --r: 2deg;
+      }
+    }
+
+    a {
+      font-size: 2.5rem;
+      text-decoration: none;
+      &:hover {
+        color: var(--red);
+      }
+      &[aria-current='page'] {
+        color: var(--red);
+      }
+    }
+  }
+`
 
 const Nav = (props) => (
-  <nav>
+  <NavStyled>
     <ul>
       <li>
         <Link to="/">Hot Now</Link>
@@ -18,16 +63,18 @@ const Nav = (props) => (
         <Link to="/pizzas/">Pizza Menu</Link>
       </li>
       <li>
-        <Link to="/">LOGO</Link>
+        <Link to="/">
+          <Logo />
+        </Link>
       </li>
       <li>
         <Link to="/slice-masters">Our Chefs</Link>
       </li>
       <li>
-        <Link to="/order">Your Order</Link>
+        <Link to="/order">Order Up!</Link>
       </li>
     </ul>
-  </nav>
+  </NavStyled>
 )
 
 Nav.propTypes = {}
